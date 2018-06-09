43392796ebd92ce847dba60e07fe004b57f10f39
Rubén <hey@rubenvara.io>
2018-06-09 13:01:24 +0200

    basic db connections and style

--- controllers/appController.js
+++ controllers/appController.js
@@ -16,6 +16,28 @@ exports.registerTip = async (req, res) => {
   res.redirect('/');
 };
 
-exports.editTip = async (req, res) => {
-  res.render('editTip');
+exports.chooseTipToUpdate = async (req, res) => {
+  const tips = await Tip.find();
+  res.render('chooseTip', { tips });
+};
+
+exports.renderUpdateForm = async (req, res) => {
+  const tip = await Tip.findOne( { _id: req.body.tips });
+  res.render('update', { tip });
+};
+
+exports.updateTip = async (req, res) => {
+  const tip = await Tip.findOneAndUpdate({ _id: req.params.tipId }, req.body, {
+    new: true,
+    runValidators: true
+  }).exec();
+  req.flash('success', 'successfully updated that tip');
+  res.redirect('/');
+};
+
+exports.deleteTip = async (req, res) => {
+  // res.json(req.body);
+  const tip = await Tip.find({ _id: req.body.id }).remove().exec();
+  req.flash('success', 'successfully deleted that tip');
+  res.redirect('/');
 };
--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,12 @@ const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(appController.renderMain));
 
-router.get('/new-tip', catchErrors(appController.newTip));
-router.post('/new-tip-submit', catchErrors(appController.registerTip));
+router.get('/new', appController.newTip);
+router.post('/add-tip', catchErrors(appController.registerTip));
 
-router.get('/edit-tip', catchErrors(appController.editTip));
+router.get('/update', catchErrors(appController.chooseTipToUpdate));
+router.post('/update-form', catchErrors(appController.renderUpdateForm));
+router.post('/add-tip/:tipId', catchErrors(appController.updateTip));
+router.post('/delete', catchErrors(appController.deleteTip));
 
 module.exports = router;
